df22e5a70c23e389269d1f9aed18725b6f673a6d
发发 <kiddnesta@gmail.com>
2017-09-10 19:12:35 +0800

    廖雪峰fiter作业

---
+++ filter.py
@@ -0,0 +1,10 @@
+def is_palindrome(n):
+ 	L=str(n)
+ 	for i in range(len(L)):
+ 		if (L[i]==L[-i-1])==False:
+ 			return False
+ 		else :
+ 			pass
+ 	return True
+output = filter(is_palindrome, range(1, 10000))
+print(list(output))
